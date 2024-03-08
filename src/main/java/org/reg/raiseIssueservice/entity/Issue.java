1e86ff3d80c5bdb7200372745a2c23c6fb2a9f0e
sharonvijay <sharonvijay2003@gmail.com>
2024-03-08 11:27:11 +0530

    raiseIssue-service

--- src/main/java/org/reg/raiseIssueservice/entity/Issue.java
+++ src/main/java/org/reg/raiseIssueservice/entity/Issue.java
@@ -1,6 +1,7 @@
 package org.reg.raiseIssueservice.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,7 +18,9 @@ public class Issue {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-    private String raisedBy;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User raisedBy;
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
     private Date raisedAt;
     private String status;
